404c0db95287c1fca8b56da0f3eda8bb9f5ae6d8
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-05-11 15:02:59 +1000

    removed geolocator for weather widget
    readding geolocator later

--- src/components/weather-widget.js
+++ src/components/weather-widget.js
@@ -19,45 +19,22 @@ class WeatherWidget extends LitElement {
     super();
     this.header = 'Widget';
     const today = Date()
-    function geoFindMe() {
-        const status = document.querySelector("#status");
-        const mapLink = document.querySelector("#map-link");
-      
-        mapLink.href = "";
-        mapLink.textContent = "";
 
-        function success(position) { //gets location co-ordinates and stores them
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-        }
-
-        function error() { // if geolocator does not work, set location to Sydney
-            const latitude = -33.87
-            const longitude = 151.21
-        }
-
-        if (!navigator.geolocation) { //checks if geolocator is compatible with browser
-            status.textContent = "Geolocation is not supported by your browser";
-        } else {
-            status.textContent = "Locating…";
-            navigator.geolocation.getCurrentPosition(success, error);
-        }
-    }
     // setting the url to use when fetching data from API
-    const apiURL =  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&forecast_days=1&timezone=auto`
+    const apiURL =  `https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&forecast_days=1&timezone=auto`
 
     fetch (apiURL)
         .then(response => response.text())
         .then(data => {
             //The weather data received is set to variables 
-             var timezone = data['timezone'];
-             var temp = data['daily']['tempurature'];
-             var windspeed = data['daily']['windspeed'];
-             var maxTemp = data['daily']['data.temperature_2m_max'][0];
-             var minTemp = data['daily']['data.temperature_2m_min'][0];
-             var precipSum = data['daily']['data.precipitation_sum'][0];
-             var sunrise = data['daily']['sunrise'][0];
-             var sunset = data['daily']['sunset'][0];
+             let timezone = data['timezone'];
+             let temp = data['daily']['tempurature'];
+             let windspeed = data['daily']['windspeed'];
+             let maxTemp = data['daily']['data.temperature_2m_max'][0];
+             let minTemp = data['daily']['data.temperature_2m_min'][0];
+             let precipSum = data['daily']['data.precipitation_sum'][0];
+             let sunrise = data['daily']['sunrise'][0];
+             let sunset = data['daily']['sunset'][0];
         })
           .catch(error => console.error(error));
     
@@ -68,6 +45,16 @@ class WeatherWidget extends LitElement {
         <h3>${this.header}</h3>
         <title>Todays Weather</title>
         <p>${this.textContent}</p>
+        <ul>
+          <li>Timezone: ${this.timezone}</li>
+          <li>Temp: ${this.temp}</li>
+          <li>Windspeed: ${this.windspeed}</li>
+          <li>maxTemp: ${this.maxTemp}</li>
+          <li>minTemp: ${this.minTemp}</li>
+          <li>precipSum: ${this.precipSum}</li>
+          <li>sunrise: ${this.sunrise}</li>
+          <li>sunset: ${this.sunset}</li>
+        </ul>
     `;
   }
 }
