6bfa7bd1de5f5b11498109ed32d97f0ab9b9b4a3
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-05-12 00:50:36 +1000

    Completed fixed weather widget
    now working on geolocation API

--- src/components/weather-widget.js
+++ src/components/weather-widget.js
@@ -3,7 +3,12 @@ import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/co
 class WeatherWidget extends LitElement {
   static properties = {
     header: { type: String },
-    textContent: {type: String}
+    timezone: { type: String },
+    maxTemp: { type: String },
+    minTemp: { type: String },
+    precipSum: { type: String },
+    sunrise: { type: String },
+    sunset: { type: String }
   }
 
   static styles = css`
@@ -17,46 +22,52 @@ class WeatherWidget extends LitElement {
 
   constructor() {
     super();
-    this.header = 'Widget';
-    const today = Date()
+    this.header = "Today's Weather";
+    this.timezone = '';
+    this.maxTemp = '';
+    this.minTemp = '';
+    this.precipSum = '';
+    this.sunrise = '';
+    this.sunset = '';
 
     // setting the url to use when fetching data from API
     const apiURL =  `https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&forecast_days=1&timezone=auto`
 
     fetch (apiURL)
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
             //The weather data received is set to variables 
-             let timezone = data['timezone'];
-             let temp = data['daily']['tempurature'];
-             let windspeed = data['daily']['windspeed'];
-             let maxTemp = data['daily']['data.temperature_2m_max'][0];
-             let minTemp = data['daily']['data.temperature_2m_min'][0];
-             let precipSum = data['daily']['data.precipitation_sum'][0];
-             let sunrise = data['daily']['sunrise'][0];
-             let sunset = data['daily']['sunset'][0];
+             this.timezone = data['timezone'];
+             this.maxTemp = data['daily']['temperature_2m_max'][0];
+             this.minTemp = data['daily']['temperature_2m_min'][0];
+             this.precipSum = data['daily']['precipitation_sum'][0];
+             this.sunrise = data['daily']['sunrise'][0];
+             this.sunset = data['daily']['sunset'][0];
+             this.tempUnit = data['daily_units']['temperature_2m_max']
+             this.precipUnit = data['daily_units']['precipitation_sum'];
+             console.log(this.timezone, this.maxTemp, this.minTemp, this.precipSum, this.sunrise, this.sunset);
         })
           .catch(error => console.error(error));
+    
     
   }
 
   render() {
+    if (this.timezone == '')
+      return html`Loading Weather Data...`;
+    console.log(this.timezone, this.maxTemp, this.minTemp, this.precipSum, this.sunrise, this.sunset);
     return html`
-        <h3>${this.header}</h3>
-        <title>Todays Weather</title>
-        <p>${this.textContent}</p>
-        <ul>
-          <li>Timezone: ${this.timezone}</li>
-          <li>Temp: ${this.temp}</li>
-          <li>Windspeed: ${this.windspeed}</li>
-          <li>maxTemp: ${this.maxTemp}</li>
-          <li>minTemp: ${this.minTemp}</li>
-          <li>precipSum: ${this.precipSum}</li>
-          <li>sunrise: ${this.sunrise}</li>
-          <li>sunset: ${this.sunset}</li>
-        </ul>
+      <h3>${this.header}</h3>
+      <title>Todays Weather</title>
+      <p>Timezone: ${this.timezone}<br>
+      maxTemp: ${this.maxTemp}${this.tempUnit}<br>
+      minTemp: ${this.minTemp}${this.tempUnit}<br>
+      precipSum: ${this.precipSum}${this.precipUnit}<br>
+      sunrise: ${this.sunrise.substring(11,)}<br>
+      sunset: ${this.sunset.substring(11,)}</p>
+      <p>${this.textContent}</p>
     `;
-  }
+    }
 }
 
 customElements.define('weather-widget', WeatherWidget);
